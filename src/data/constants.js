2ee2a95bb5f3a305daff15814ffcb038283442b7
JJ512-ishere <119097879+JJ512-ishere@users.noreply.github.com>
2024-07-02 12:39:11 +0400

    Initial Commit

---
+++ src/data/constants.js
@@ -0,0 +1,310 @@
+export const Bio = {
+    name: "Joel Joseph",
+    roles: [
+      "CS Engineering Student",
+      "Alpha MLSA Ambassador",
+      "ML Enthusiast",
+      "Programmer",
+    ],
+    description:
+      "Final Year Computer Science Engineering Student at BITS Pilani,Dubai Campus.Detail-oriented and self driven, adept at testing software products, developing and implementing platforms. Having a comprehensive understanding of Python, Java,C and excellent knowledge of programming, debugging, and management.",
+    github: "https://github.com/JJ512-ishere",
+    resume:
+      "https://drive.google.com/file/d/1Jzu_vm1X-201nf_b0OUjC1v8TT7t7hVf/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/joel-joseph-30a289215/",
+    insta: "https://www.instagram.com/joelj.05/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "Oracle Database",
+          image: "https://media.licdn.com/dms/image/D5612AQGTzGGnu886Vw/article-cover_image-shrink_720_1280/0/1680581578662?e=2147483647&v=beta&t=Jc3XYEXXsZNfua5Dy96AX17G59H2LrEj4mcGb6rWt60",
+        },
+        {
+            name: "PHP MyAdmin",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/PhpMyAdmin_logo.svg/1200px-PhpMyAdmin_logo.svg.png",
+          },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Capcut",
+          image:
+            "https://images-eds-ssl.xboxlive.com/image?url=4rt9.lXDC4H_93laV1_eHM0OYfiFeMI2p9MWie0CvL99U4GA1gf6_kayTt_kBblFwHwo8BW8JXlqfnYxKPmmBaQDG.nPeYqpMXSUQbV6ZbDAIgQ7KqpBVwBuUDaLqt9_hZPXOmNdAPbs1q1SGvczIJ.c6LtW5g9hEdXZ9kmdixA-&format=source&h=464",
+        },
+        {
+          name: "PowerBI",
+          image:
+            "https://miit.edu.mu/wp-content/uploads/2023/08/Microsoft-Power-BI-Symbol.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://yt3.googleusercontent.com/X-rhKMndFm9hT9wIaJns1StBfGbFdLTkAROwm4UZ3n9ucrBky5CFIeeZhSszFXBgQjItzCD0SA=s900-c-k-c0x00ffffff-no-rj",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://media.licdn.com/dms/image/C4D0BAQHNrbXZ_vDOow/company-logo_200_200/0/1630580598393?e=2147483647&v=beta&t=3Ue68GGUhknXwiGyxmRDNMxx0kejz5pqtD4FwPl9wGI",
+      role: "Outreach Head",
+      company: "Microsoft Tech Club,BPDC",
+      date: "June 2024 - Present",
+      desc: "Heeading External Collaborations and the the Outreach Activities to hel the growth of the club",
+      skills: [
+        "Slack",
+        "Effective Communication",
+        "Team Leadership",
+      ],
+    },
+    {
+      id: 0,
+      img: "https://pbs.twimg.com/profile_images/1268202810283225095/RPVzfN-t_400x400.png",
+      role: "Alpha Learn Ambassador",
+      company: "Microsoft Learn",
+      date: "October 2023 - Present",
+      desc: "Engaging with faculty and students to showcase the benefits of Microsoft tools for education and innovation and also completing projects and hosting events on campus.",
+      skills: [
+        "PowerBI",
+        "MS Office",
+        "Fundamentals of Azure",
+      ],
+    },
+    {
+      id: 0,
+      img: "https://media.licdn.com/dms/image/C4D0BAQHNrbXZ_vDOow/company-logo_200_200/0/1630580598393?e=2147483647&v=beta&t=3Ue68GGUhknXwiGyxmRDNMxx0kejz5pqtD4FwPl9wGI",
+      role: "Marketing Head",
+      company: "Microsoft Tech Club",
+      date: "June 2023 - June 2024",
+      desc: "Developed Marketing Strategies and Content to Promote Clubs Events and also Controlled Social Media Activities for engagement with members.",
+      skills: [
+        "Social Media Management",
+        "Capcut",
+        "IMovie",
+        "Davinci Resolve(Basic)",
+      ],
+    },
+    {
+      id: 1,
+      img: "https://media.licdn.com/dms/image/C510BAQHs-3DlfatOkA/company-logo_200_200/0/1630587213921?e=2147483647&v=beta&t=hImtILIvNUqIz5mEQMTZ-nvl0CtywnJ70CNmMVlqRxI",
+      role: "Business Development Intern",
+      company: "Resource Log",
+      date: "January 2024 - February 2024",
+      desc: "CV Sourcing of Ideal Candidates for respective roles of Clients and Communicating with Candidates and Potential Clients through the recruitment cycle.",
+      skills: [
+        "Linkedin",
+        "Effective Communication",
+      ],
+    },
+    {
+      id: 3,
+      img: "https://fidelityunited.ae/wp-content/uploads/2023/03/logo.jpg",
+      role: "IT Intern",
+      company: "Fidelity United",
+      date: "June 2023 - August 2023",
+      desc: "Developed Documentation Highlighting the Insurance Procedure for the Claims and Underwriting Departments for Motor Insurance in the UAE,Developed Dynamic Company Reports using SAP Crystal Reports Software for Company Invoices and Claims Reports",
+      skills: [
+        "Oracle Databases",
+        "SAP Crystal Reports"
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ364gPhizS0_ZwRKG23g10-buxMhDExnliyA&s",
+      school: "BITS Pilani Dubai Campus",
+      date: "Sep 2021 - Sep 2025",
+      grade: "9.68 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Bits Pilani Dubai Campus. I have completed 6 semesters and have a CGPA of 9.68. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+      degree: "Bachelor of Technology - BTech, Computer Science Engineering",
+    },
+    {
+      id: 1,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb3dfPnYxHEiQphd4XroMfjHLH5J_4G-hoUA&s",
+      school: "Delhi Private School,Dubai",
+      date: "Apr 2019 - Apr 2021",
+      grade: "95.1%",
+      desc: "I completed my class 12 high school education at DPS,Dubai, where I studied Science with Computer Science.",
+      degree: "Physics,Chemistry,Math,Computer Science,Marketing",
+    },
+    {
+      id: 2,
+      img: "https://pbs.twimg.com/profile_images/987687428365389825/F42jiM8p_400x400.jpg",
+      school: "JSS Private School,Dubai",
+      date: "Apr 2014 - Apr 2019",
+      grade: "89.9%",
+      desc: "I completed my class 10 education at JSSPS,Dubai.",
+      degree: "Science,Math,Computer,SST,Hindi,English",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 10,
+      title: "Weather App",
+      date: "Jun 2024-Jun 2024",
+      description:
+        "A weather app using HTML,CSS,Java Script and a OpenWeather API",
+      images:"https://private-user-images.githubusercontent.com/119097879/344920542-1ee98055-f4f1-45bf-81e0-b095b69546fa.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTk5MDg2MjcsIm5iZiI6MTcxOTkwODMyNywicGF0aCI6Ii8xMTkwOTc4NzkvMzQ0OTIwNTQyLTFlZTk4MDU1LWY0ZjEtNDViZi04MWUwLWIwOTViNjk1NDZmYS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNzAyJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDcwMlQwODE4NDdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1jMzFhMTFkOTA0YWIyN2RlN2FkYjNjOTFiYzU3NDRlMTJlODhlOWNjMTBmNDRlOTVjNzg1ZjQ2Yzc1ZGIwYWNjJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.ttzw1N0jAlQGTdkUeGT3ERP1yaIXHt8c-RCcLDiXN54",
+      tags: ["JavaScript", "HTML", "CSS","API's"],
+      category: "web app",
+      github: "https://github.com/JJ512-ishere/chess-engine",
+    },
+    {
+      id: 9,
+      title: "Customer Churn Prediction",
+      date: "Feb 2024 - May 2024",
+      description:
+        "A Customer Churn Prediction Model for an E-Commerce Website with noticeable class imbalance between Churn and Non-Churn in the dataset.Initially when using our CNN-LSTM model we only had an accuracy of about 84%.After using GAN(Generative Adversial Networks) on the dataset we were able to handle the class imbalance and when passed through our model we were able to achieve an accuracy of 97%.",
+      tags: [
+        "Neural Networks",
+        "CNN",
+        "GAN",
+        "MathPlotLib"
+      ],
+      category: "machine learning",
+      github: "https://github.com/JJ512-ishere/Customer-Churn-Predction",
+    },
+    {
+      id: 0,
+      title: "Travel Recommendation System - An Integrated CF Approach",
+      date: "Sep 2023 - Dec 2023",
+      description:
+        "Our Model  shows that using the combined features of LBSN data and TripAdvisor data to generate personalized tourist recommendations are conducive for efficient travel planning.",
+      tags: ["Pandas", "LSBN", "WebScraping"],
+      category: "machine learning",
+      github: "https://github.com/JJ512-ishere/Tourist-Recommender-System",
+    },
+    {
+      id: 1,
+      title: "Football Predictor",
+      date: "Jan 2024-Feb 2024",
+      description:
+        "A premier league football predictor using webscrapped football stats to make game predictions for the 23-24 seasons",
+      image:"",
+      tags: [
+        "Python",
+        "Pandas",
+        "Beautiful Soup",
+        "MathPlotLib"
+      ],
+      category: "machine learning",
+      github: "https://github.com/JJ512-ishere/Football-Predictor-",
+    },
+    {
+      id: 2,
+      title: "Movie Recomendation System",
+      date: "Jan 2024 - Feb 2024",
+      description:
+        "A movie recomendation system based on content based filtering that compares movies with similar words in their descriptions to the movie entered.",
+      tags: ["Python", "Content Based Filtering", "Jupyter Notebook", "Streamlit"],
+      category: "machine learning",
+      github: "https://github.com/JJ512-ishere/movie_reccomendation_system",
+    },
+    {
+      id: 3,
+      title: "Insurance Game",
+      date: "Jun 2023 - Sep 2023",
+      description:
+        "A simple game to explain the basic of insurance and protecting assets from uncertain events.",
+      tags: ["HTML", "CSS", "MySQL", "PHPMyadmin"],
+      category: "web app",
+      github: "https://github.com/JJ512-ishere/insurance_game",
+    },
+    {
+      id: 10,
+      title: "Sports HomePage",
+      date: "Jun 2023 - Jul 2023",
+      description:
+        "A basic home page for a Sports Store",
+      tags: ["HTML", "CSS"],
+      category: "web app",
+      github: "https://github.com/JJ512-ishere/SportsHomePage",
+    },
+    {
+      id: 4,
+      title: "Campus Management System",
+      date: "Feb 2023-May 2023",
+      description:
+        "A Campus Management System utilzing distributed databses to oversee all Campus Activities",
+      tags: ["Angular", "MySQL"],
+      category: "web app",
+      github: "https://github.com/JJ512-ishere/dbs-campus-sytem",
+    },
+    {
+      id: 5,
+      title: "Java Chess Engine",
+      date: "Oct 2022- Dec 2022",
+      description:
+        "Chess board game using bit boards using advanced OOP concepts with Java.",
+      tags: ["Java", "GUI's", "OOP's"],
+      category: "web app",
+      github: "https://github.com/JJ512-ishere/chess-engine",
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
